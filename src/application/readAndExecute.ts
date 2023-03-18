dd47ca31d9b5aac87de059c6814a3ab736f718df
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2023-03-18 16:18:28 -0300

    feat: implemented entryPoint and path checkers

--- src/application/readAndExecute.ts
+++ src/application/readAndExecute.ts
@@ -1,9 +1,16 @@
-import { readFileSync } from "fs";
+import { readFileSync, existsSync } from "fs";
 import { join } from "path";
 import { Program } from "./types";
 
 function readAndExecute (identifier: string, args: string[]) {
     const programPath = join("./", identifier, "program.json");
+    const programExists = existsSync(programPath);
+
+    if (!programExists) {
+        console.error("The specified path doesn't exist.");
+        process.exit();
+    }
+    
     const programRaw = readFileSync(programPath).toString();
     const program = JSON.parse(programRaw) as Program;
 
@@ -24,6 +31,18 @@ function readAndExecute (identifier: string, args: string[]) {
                 console.log(commandText);
             }
         }
+
+        return;
+    }
+
+    const inputCommand = args[0];
+
+    if (program.commands.find(command => command.entryPoint === inputCommand)) {
+
+    }
+    else {
+        console.error("The specified command doesn't exist.");
+        process.exit();
     }
 }
 
