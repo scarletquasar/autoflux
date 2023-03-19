2bed8a8a46b7db132941b98a0df5ca02d1441657
Victória Rose <70824102+victoriaquasar@users.noreply.github.com>
2023-03-19 00:22:32 -0300

    refactor: normalized file names and avoided main thread exit

---
+++ src/application/command-handler.ts
@@ -0,0 +1,90 @@
+import { readFileSync, existsSync } from "fs";
+import { join } from "path";
+import { SpawnOptionsWithoutStdio, spawn, ChildProcessWithoutNullStreams } from "child_process";
+import { Command, Program } from "./types";
+
+class CommandHandler {
+    public static readAndExecute (identifier: string, args: string[]) {
+        const programPath = join("./", identifier, "program.json");
+        const programExists = existsSync(programPath);
+    
+        if (!programExists) {
+            console.error("The specified path doesn't exist.");
+            process.exit();
+        }
+        
+        const programRaw = readFileSync(programPath).toString();
+        const program = JSON.parse(programRaw) as Program;
+    
+        const showHelpCondition = args.length === 0 || (args.length === 1 && args[0] == "--help");
+        
+        if (showHelpCondition) {
+            if (program.help.textEnabled) {
+                console.log(program.help.text);
+            }
+    
+            if (program.help.tipsEnabled) {
+                console.log("Commands:\n");
+                for(const command of program.commands) {
+                    let commandText = `Name: ${command.entryPoint}`
+                        + ` | Type: ${command.type}`
+                        + ` | Example Usage: ${command.exampleUsage}`;
+    
+                    console.log(commandText);
+                }
+            }
+    
+            return;
+        }
+    
+        const inputCommand = args[0];
+        const command = program.commands.find(command => command.entryPoint === inputCommand);
+        if (command) {
+            this.handleExecution(command, args.slice(1));
+        }
+        else {
+            console.error("The specified command doesn't exist.");
+            process.exit();
+        }
+    }
+
+    private static handleExecution(command: Command, args: string[]) {
+        const childSpawnOptions: SpawnOptionsWithoutStdio = { stdio: "inherit" } as unknown;
+        let eventEmitter: ChildProcessWithoutNullStreams;
+    
+        switch(command.type) {
+            case "shell":
+                eventEmitter = spawn(
+                    command.instructions[0], 
+                    command.instructions.slice(1).concat(args), 
+                    childSpawnOptions
+                );
+                break;
+            case "ps1-file":
+                eventEmitter = spawn(
+                    "pwsh", 
+                    command.instructions.concat(args), 
+                    childSpawnOptions
+                );
+                break;
+            case "sh-file":
+                eventEmitter = spawn(
+                    "sh", 
+                    command.instructions.concat(args), 
+                    childSpawnOptions
+                );
+                break;
+            case "js-node-file":
+                eventEmitter = spawn(
+                    "node", 
+                    ["-e"].concat(command.instructions).concat(args), 
+                    childSpawnOptions
+                );
+                break;
+        }
+    
+        eventEmitter.on("data", console.log);
+    }
+}
+
+export { CommandHandler }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { readAndExecute } from "./application/methods";
+import { CommandHandler } from "./application/command-handler";
 
 const args = process.argv.slice(2);
 
-if (args.length > 0) {
-    readAndExecute(args[0], args.slice(1));
+if (args.length) {
+    CommandHandler.readAndExecute(args[0], args.slice(1));
+}
+else {
+    console.error("Invalid input, please try again with valid arguments.");
 }
